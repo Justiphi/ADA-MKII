0de232d3573c606f61f810bb2392c7e1c9e097af
Travis Wylie <traviswylie58@gmail.com>
2023-03-25 16:51:02 +1300

    init commit - base MAUI project

---
+++ ADA-MKII/ADA-MKII/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ADA_MKII;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ ADA-MKII/ADA-MKII/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ADA_MKII;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
