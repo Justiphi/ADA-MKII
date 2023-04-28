54deab69b0b224c12dfc8dec0a50a929a12498c6
Travis Wylie <traviswylie58@gmail.com>
2023-04-28 20:54:14 +1200

    efcore SQLite nuget package and connection string

--- ADA-MKII/ADA-MKII-Data/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ADA_MKII_Data
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ ADA-MKII/ADA-MKII-Data/Model.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using System.Collections.Generic;
+using System;
+using System.IO;
+
+namespace ADA_MKII_Data
+{
+    public class Model
+    {
+        public class DataContext : DbContext
+        {
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlite($"Data Source={Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\ADA-MKII\\ADA.db;Version=3;");
+            }
+        }
+    }
+}
